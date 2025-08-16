b38f20af285f48875b37d817392d254ed52b47cd
Luka Popovic <lukapopovic0111@gmail.com>
2025-08-16 15:24:01 +0200

    synchronous programing

---
+++ script.js
@@ -0,0 +1,12 @@
+'use strict';
+// 1.Synchronous (Synchronous Programming)!(jedno za drugim,liniju po liniju se izvrsava)
+console.log('1. Preheat the oven to 350°F (180°C).');
+console.log('2. Chop half the chocolate into chunks & melt in microwave.');
+console.log('3. Mix butter & sugar.');
+console.log('4. Beat in the eggs and vanilla for 1-2 minutes.');
+console.log('5. the melted chocolate.');
+console.log(
+  '6. Fold in the other half of chocolate chunks, then transfer the batter to baking dish.'
+);
+console.log('7. Bake for 20-25 minutes,then let cool completely.');
+console.log('8. slice, serve & enjoy!');
