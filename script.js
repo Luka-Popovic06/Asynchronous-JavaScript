e463b980a268f01537ce1752a8fbbae3e8e79a54
Luka Popovic <lukapopovic0111@gmail.com>
2025-08-17 22:15:12 +0200

    Implement sequential cooking steps using nested callbacks

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ console.log('8. slice, serve & enjoy!');
 /*isti princip samo sto se ne prikazuje redosledom kojim smo napisali
 prvo se prikaze 8. 1. 3. itd. 
 */
-setTimeout(() => {
+/*setTimeout(() => {
   console.log('1. Preheat the oven to 350°F (180°C).');
 }, 1000);
 setTimeout(() => {
@@ -39,4 +39,70 @@ setTimeout(() => {
 }, 1800);
 setTimeout(() => {
   console.log('8. slice, serve & enjoy!');
-}, 500);
+}, 500);*/
+//
+function task1(callback) {
+  setTimeout(() => {
+    console.log('1. Preheat the oven to 350°F (180°C).');
+    callback();
+  }, 1000);
+}
+function task2(callback) {
+  setTimeout(() => {
+    console.log('2. Chop half the chocolate into chunks & melt in microwave.');
+    callback();
+  }, 2000);
+}
+function task3(callback) {
+  setTimeout(() => {
+    console.log('3. Mix butter & sugar.');
+    callback();
+  }, 1500);
+}
+function task4(callback) {
+  setTimeout(() => {
+    console.log('4. Beat in the eggs and vanilla for 1-2 minutes.');
+    callback();
+  }, 3000);
+}
+function task5(callback) {
+  setTimeout(() => {
+    console.log('5. the melted chocolate.');
+    callback();
+  }, 4000);
+}
+function task6(callback) {
+  setTimeout(() => {
+    console.log(
+      '6. Fold in the other half of chocolate chunks, then transfer the batter to baking dish.'
+    );
+    callback();
+  }, 3500);
+}
+function task7(callback) {
+  setTimeout(() => {
+    console.log('7. Bake for 20-25 minutes,then let cool completely.');
+    callback();
+  }, 1800);
+}
+function task8(callback) {
+  setTimeout(() => {
+    console.log('8. slice, serve & enjoy!');
+    callback();
+  }, 500);
+}
+task1(() => {
+  task2(() => {
+    task3(() => {
+      task4(() => {
+        task5(() => {
+          task6(() => {
+            task7(() => {
+              task8(() => {});
+            });
+          });
+        });
+      });
+    });
+  });
+});
