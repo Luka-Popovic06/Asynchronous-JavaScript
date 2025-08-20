38b049c70088ccfcdd07bc130517323dd5f214e5
Luka Popovic <lukapopovic0111@gmail.com>
2025-08-20 13:37:52 +0200

    Add pending Promise example with random resolve/reject

--- promises.js
+++ promises.js
@@ -1,10 +1,22 @@
 //Promise
-const promise = new Promise((resolve, reject) => {
+/*const promise = new Promise((resolve, reject) => {
   const allWentWell = true;
   if (allWentWell) {
     resolve('All things went well!');
   } else {
     reject('Oop! Something went wrong!');
   }
+});*/
+
+//Pending
+const promise = new Promise((resolve, reject) => {
+  const randomNumber = Math.floor(Math.random() * 10);
+  setTimeout(() => {
+    if (randomNumber < 4) {
+      resolve('Well Done! You Guessed Right!');
+    } else {
+      reject('Oops! You Guessed Wrong! Unlucky.');
+    }
+  });
 });
 console.log(promise);
